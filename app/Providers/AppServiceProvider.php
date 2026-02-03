629165a5c50b6d72463936c44ec7baac697e3baf
Oatto1 <weeraphon.oatto@outlook.com>
2026-02-03 19:01:56 +0700

    fig

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if($this->app->environment('production') || $this->app->environment('staging')) {
+            \URL::forceScheme('https');
+        }
     }
 }
