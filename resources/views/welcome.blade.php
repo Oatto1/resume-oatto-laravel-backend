a2940a3004a35c79718845aa84ae793a81dd92bb
Oatto1 <weeraphon.oatto@outlook.com>
2026-02-21 14:39:01 +0700

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -293,7 +293,7 @@
                                 <div class="flex gap-4 mb-4">
                                     <div class="flex-shrink-0">
                                         @if($education->image)
-                                            <img src="{{ asset('storage/' . $education->image) }}" alt="{{ $education->localized('school') }}" class="h-16 w-16 rounded-xl object-cover shadow-sm">
+                                            <img src="{{ asset('storage/' . $education->image) }}" alt="{{ $education->localized('school') }}" class="h-16 w-16 rounded-xl object-contain shadow-sm">
                                         @else
                                             <div class="h-16 w-16 rounded-xl bg-gray-100 flex items-center justify-center text-gray-400">
                                                 <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path></svg>
